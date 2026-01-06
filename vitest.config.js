214bb9fc2a42afaf6afde6924ae09cb069bd9af7
ebbestankiewcz <ebbe2002@me.com>
2026-01-06 12:20:26 +0100

    Exclude e2e tests from Vitest

--- vitest.config.js
+++ vitest.config.js
@@ -3,5 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/tests/e2e/**",
+      "**/playwright-report/**",
+      "**/test-results/**",
+    ],
   },
 });
